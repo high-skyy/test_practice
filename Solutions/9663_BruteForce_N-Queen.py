3f4f6618ef1cdb64139b3a78aed53046485b2300
sanghyunseo97 <joykupid@korea.ac.kr>
2023-01-02 17:41:41 +0900

    구현 뱀

--- Solutions/9663_BruteForce_N-Queen.py
+++ Solutions/9663_BruteForce_N-Queen.py
@@ -1,36 +1,14 @@
 # N-Queen 문제
 
 import sys
-from itertools import combinations
+from itertools import permutations
 
-
+# row + col // col - row
 N = int(sys.stdin.readline())
-loc_list = [[row, col] for row in range(N) for col in range(N)]
+queen_list = [-1 for i in range(N)]
+global count
 
-count = 0
-for comb in combinations(loc_list, N):
-    vertical_list = []
-    horizontal_list = []
-    y_x = []
-    y_x_reverse = []
-    possible = True
-    for row, col in comb:
-        if row in horizontal_list:
-            possible = False
-            break
-        if col in vertical_list:
-            possible = False
-            break
-        if row + col in y_x:
-            possible = False
-            break
-        if col - row in y_x_reverse:
-            possible = False
-            break
-        horizontal_list.append(row)
-        vertical_list.append(col)
-        y_x.append(row + col)
-        y_x_reverse.append(col - row)
-    if possible:
+def nqueen(cur_index):
+    global count
+    if cur_index == n:
         count += 1
-print(count)
--- test.py
+++ test.py
@@ -1,38 +1,4 @@
-# 3 연산 문자 (+, -, *) 우선순위 -> 큰 숫자
-# 계산된 결과가 음수 -> 절댓 값
-
-from itertools import permutations
-import re
-
-
-def solution(expression):
-    answer = 0
-    # 연산자 순서대로 (permutations)
-    # "100-200*300-500+20"
-    operand_list = list(map(int, re.findall("[0-9]+", expression)))
-    operator_list = re.findall("[\W]", expression)
-    new_list = [operand_list[0]]
-    for i in range(len(operator_list)):
-        new_list.append(operator_list[i])
-        new_list.append(operand_list[i])
-    # print(new_list)
-    max_value = 0
-    for op_seq in permutations(operator_list):
-        test_list = new_list[:]
-        for op in op_seq:
-            while op in test_list:
-                index = test_list.index(op)
-                first = test_list.pop(index - 1)
-                second = test_list.pop(index - 1)
-                third = test_list.pop(index - 1)
-                if op == "-":
-                    test_list.insert(index - 1, first - third)
-                elif op == "+":
-                    test_list.insert(index - 1, first + third)
-                else:
-                    test_list.insert(index - 1, first * third)
-            print(test_list)
-        if test_list[0] > max_value:
-            max_value = test_list[0]
-    return max_value
-solution("100-200*300-500+20")
+test = [1,2]
+test.pop(0)
+test.pop(0)
+test.pop(0)
