79fa22c8aeb3ceffde3569fcff06119d3331d0d4
SangHyun <joykupid@korea.ac.kr>
2023-01-12 22:20:22 +0900

    SQL

--- SQL.md
+++ SQL.md
@@ -1,5 +1,71 @@
 # SQL
+
+## TIP
+겹치는 것이 없으면 굳이 I.뭐를 쓸 필요가 없음.
+
+## 이중 order by
+결과 집합을 여러 열로 정렬하려면 ORDER BY 절에 ,(쉼표) 로 구분된 열 목록을 지정합니다.
+
 ```
-SELECT ID, NAME, HOST_ID FROM (SELECT HOST_ID FROM (SELECT HOST_ID, COUNT(ID) AS num FROM PLACES GROUP BY HOST_ID) AS A
-WHERE num >= 2) AS B NATURAL JOIN PLACES ORDER BY ID;
+SELECT * FROM 테이블 ORDER BY STATUS DESC, CREATED_AT;
 ```
+> 먼저 결과 집합을 'status'값을 기준으로 내림차순으로 정렬합니다. 그런 다음 정렬 된 결과 집합을
+> status 값이 같은 경우 'createdAt' 값을 기준으로 오름차순으로 정렬합니다.
+
+- [Reference](https://dar0m.tistory.com/60)
+
+## DATE_FORMAT 함수
+이 함수는 DATETIME의 TYPE을 가진 칼럼의 형식을 수정, 지정해 주는 함수이다.
+- date의 type을 가진 값도 like가 사용이 가능하다.
+```
+DATE_FORMAT("날짜값, 날짜칼럼", "%Y");
+```
+뒤에 "%Y" 등은 형식을 지정해준다.
+ex) %Y/%M/%D
+- Format Description
+  - %Y : 연도를 4자리로
+  - %y : 연도를 2자리로
+  - %M : 달 이름을 Full Name으로 (March)
+  - %m : 달 이름을 00 ~ 12 숫자로
+  - %D : 일 이름을 1st... 등으로
+  - %d : 일 이름을 숫자로 
+
+## Inner join 과 natural join의 차이
+- Inner join
+  - default join
+  - join 조건에서 동일한 값이 있는 행 만을 반환한다.
+  - 반드시 ON 절을 써주어야 한다.
+- Natural join
+  - join 조건에서 동일한 이름을 갖는 모든 칼럼들에 대해 equi join이 수행됨
+  - join이 되는 테이블의 데이터 도메인과 칼럼명 칼럼값이 동일해야 하는 제약 조건이 있다.
+
+## 명시적 조인 표현
+```
+SELECT *
+FROM EMPLOYEE INNER JOIN DEPARTMENT
+ON EMPLOYEE.DEPARTMENTID = DEPARTMENT.DEPARTMENTID;
+```
+## 암시적 조인 표현
+```
+SELECT *
+FROM EMPLOYEE E, DEPARTMENT D
+WHERE E.DEPARTMENTID = DEPARTMENT.DEPARTMENTID;
+```
+
+## like
+```
+SELECT * FROM TABLE_NAME
+WHERE TABLE_NAME.ATTRIBUTE
+LIKE '스타%';
+```
+- 'a%' : a로 시작하는 문자 찾기
+- '%a' : a로 끝나는 문자열 조회
+- '%a%' : a문자가 포함된 값 모두 조회
+- '음_' : 음으로 시작하는 글자가 두 개인 문자열 조회
+- : 같은 경우에는 글자 수를 정해준다.
+
+## UNION ALL
+말 그대로 합집합 (aliasing이 필요가 없다)
+
+## NULL
+null 넣고 싶으면 select 부분에 null as column명으로 넣을 수 있다.
--- test.py
+++ test.py
@@ -1,48 +1,29 @@
-# 양방향 쉼터 / 산봉우리 휴식 / intensity (path 중 두 노드 사이의 최댓값)
-# 한번만 가는 것 까지 계산
-
 from collections import defaultdict
-import heapq
+def solution(s, times):
+    answer = []
+    c_1 = lambda x: int(x[0:4]) * 12 * 30 * 3600 * 24 + int(x[5:7]) * 30 * 3600 * 24 + int(x[8:10]) * 3600 * 24 + int(
+        x[11:13]) * 3600 + int(x[14:16]) * 60 + int(x[17:19])
+    c_2 = lambda x : int(x[0:2]) * 3600 * 24 + int(x[3:5]) * 3600 + int(x[6:8])*60 + int(x[9:11])
+    convert_day = 24*3600
+    cur_time = c_1(s)
+    day_list = [cur_time // convert_day]
+    for time in times:
+        add_time = c_2(time)
+        cur_time += add_time
+        day_list.append(cur_time // convert_day)
 
+    day_list = set(day_list)
+    count = 0
+    min_day = min(day_list)
+    max_day = max(day_list)
+    for d in range(min_day, max_day + 1):
+        if d not in day_list:
+            answer.append(0)
+            break
+    if not answer:
+        answer.append(1)
 
-def solution(n, paths, gates, summits):
-    answer = 0
-    INF = float('inf')
-    min_list = [INF, -1]
-    # n : 산의 지점 수, paths : 등산로 [i, j, w], gates : 출입구, summits : 산봉우리
-    path_dict = defaultdict(list)
-    for i, j, w in paths:
-        path_dict[i].append([j, w])
-        path_dict[j].append([i, w])
-    # print(path_dict)
-    for gate in gates:
-        q = [(0, gate)]  # intensity, cur
-        intensity_list = [[INF, i] for i in range(n + 1)]
-        min_intensity = INF
-        while q:
-            print(q)
-            intensity, cur = heapq.heappop(q)
-            if cur in summits:
-                min_intensity = min(min_intensity, intensity)
-            if intensity > min_intensity:
-                break
-            if intensity_list[cur][0] < intensity:
-                continue
-            for end, distance in path_dict[cur]:
-                heapq.heappush(q, (max(intensity, distance), end))
-                intensity_list[end][0] = min(intensity_list[end][0], distance)
-        intensity_list = sorted(intensity_list)
-        candidate = intensity_list[0]
-        if candidate[0] < min_list[0]:
-            min_list = [candidate[0], candidate[1]]
-        elif candidate[0] == min_list[0]:
-            if candidate[1] < min_list[1]:
-                min_list = [candidate[0], candidate[1]]
-        else:
-            continue
-
-    answer = min_list
+    answer.append(max_day - min_day + 1)
+    print(answer)
     return answer
-
-
-solution(6, [[1, 2, 3], [2, 3, 5], [2, 4, 2], [2, 5, 4], [3, 4, 4], [4, 5, 3], [4, 6, 1], [5, 6, 1]], [1,3], [5])
+solution("1997:03:14:00:00:00", ["00:00:00:00"])
